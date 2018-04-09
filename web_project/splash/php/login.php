2ecc74e80eda6b874815b14c6ac65bc121ee1428
phoenixSP <shreyasipal.in@gmail.com>
2018-04-09 16:18:43 +0530

    changes2

--- web_project/splash/php/login.php
+++ web_project/splash/php/login.php
@@ -8,21 +8,22 @@
       $myusername = mysqli_real_escape_string($db,$_POST['username']);
       $mypassword = mysqli_real_escape_string($db,$_POST['password']); 
       
-      $sql = "SELECT id FROM admin WHERE username = '$myusername' and passcode = '$mypassword'";
+      $sql = "SELECT * FROM user WHERE regNo = '$myusername' and pass = '$mypassword'";
       $result = mysqli_query($db,$sql);
       $row = mysqli_fetch_array($result,MYSQLI_ASSOC);
-      $active = $row['active'];
+      //$active = $row['active'];
       
       $count = mysqli_num_rows($result);
       
       // If result matched $myusername and $mypassword, table row must be 1 row
 		
       if($count == 1) {
-         session_register("myusername");
+         //session_register("myusername");
          $_SESSION['login_user'] = $myusername;
-         
-         header("location: welcome.php");
-      }else {
+         header("location: BS3/dashboard.html");
+
+      }
+      else {
          $error = "Your Login Name or Password is invalid";
       }
    }

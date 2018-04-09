c16033e2cccebf4168e91ed586d01df8091f1971
phoenixSP <shreyasipal.in@gmail.com>
2018-04-09 15:26:25 +0530

    changes1

--- web_project/splash/php/input_dbms.php
+++ web_project/splash/php/input_dbms.php
@@ -10,13 +10,14 @@ if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 } 
 $regno=$_POST["regno"];
+$rollno=$_POST["rollno"];
 $pass=$_POST["pass"];
 $fname=$_POST["fname"];
 $lname=$_POST["lname"];
-$Number=$_POST["Number"];
-$mail=$_POST["mail"];
-$TAdd=$_POST["TAdd"];
-$PAdd=$_POST["PAdd"];
+$mobNum=$_POST["Number"];
+$email=$_POST["mail"];
+$temAdd=$_POST["TAdd"];
+$perAdd=$_POST["PAdd"];
 $dept=$_POST["dept"];
 $cgpa=$_POST["cgpa"];
 $backlogs=$_POST["backlogs"];
@@ -24,8 +25,7 @@ $marks12=$_POST["marks12"];
 $board12=$_POST["board12"];
 $marks10=$_POST["marks10"];
 $board10=$_POST["board10"];
-$sql = "INSERT INTO Students (firstname, lastname, email)
-VALUES ($fname,$lname,$Number,$mail,$TAdd,$PAdd,$dept,$cgpa,$backlogs,$marks12,$board12,$marks10,$board12)";
+$sql = "INSERT INTO user VALUES ($regno, $rollno, $email, $mobNum, $fname,$lname,$perAdd, $dept, $cgpa,$backlogs,$board12, $marks12,$board10,$marks10,"",$email)";
 
 if ($conn->query($sql) === TRUE) {
     echo "New record created successfully";

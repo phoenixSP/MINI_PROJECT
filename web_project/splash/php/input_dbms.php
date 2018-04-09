20ce5b00f5565091d5065fbd0e015ec646f47cca
subhroneeldey <subhroneeldey4@gmail.com>
2018-04-09 15:00:28 +0530

    php created

--- web_project/splash/php/input_dbms.php
+++ web_project/splash/php/input_dbms.php
@@ -1,8 +1,8 @@
 <?php
 $servername = "localhost";
-$username = "username";
-$password = "password";
-$dbname = "myDB";
+$username = "root";
+$dbname = "jobportal";
+$password="";
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
 // Check connection

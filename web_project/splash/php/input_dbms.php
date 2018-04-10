12ef8e371a520164ce9f696f64992d619fb37383
phoenixSP <shreyasipal.in@gmail.com>
2018-04-10 09:55:30 +0530

    php changes

--- web_project/splash/php/input_dbms.php
+++ web_project/splash/php/input_dbms.php
@@ -8,7 +8,13 @@ $conn = new mysqli($servername, $username, $password, $dbname);
 // Check connection
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
-} 
+}
+//$conn_db = mysqli_select_db($conn,"test");
+/*if(!$conn_db)
+{
+    die("unable to connect to database"); 
+}*/
+
 $regno=$_POST["regno"];
 $rollno=$_POST["rollno"];
 $pass=$_POST["pass"];
@@ -25,7 +31,7 @@ $marks12=$_POST["marks12"];
 $board12=$_POST["board12"];
 $marks10=$_POST["marks10"];
 $board10=$_POST["board10"];
-$about="abbb"
+$about="abbb";
 $sql = "INSERT INTO user(regNo,rollNo,email,mobNum,fname,lname, perAdd,temAdd,dept,cgpa,backlogs,board12,marks12,board10,marks10,about,pass) VALUES ('$regno', '$rollno','$email', '$mobNum', '$fname', '$lname', '$perAdd', '$temAdd', '$dept', '$cgpa', '$backlogs', '$board12', '$marks12', '$board10', '$marks10', '$about', '$pass')";
 
 

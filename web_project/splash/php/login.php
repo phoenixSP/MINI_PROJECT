7a87e590fd915e28bcd44c6850eb0c3c81aa134d
phoenixSP <shreyasipal.in@gmail.com>
2018-04-10 19:11:05 +0530

    login changes

--- web_project/splash/php/login.php
+++ web_project/splash/php/login.php
@@ -1,14 +1,14 @@
 <?php
    include("config.php");
    session_start();
+
    
    if($_SERVER["REQUEST_METHOD"] == "POST") {
       // username and password sent from form 
       
-      $myusername = mysqli_real_escape_string($db,$_POST['username']);
-      $mypassword = mysqli_real_escape_string($db,$_POST['password']); 
-      
-      $sql = "SELECT * FROM user WHERE regNo = '$myusername' and pass = '$mypassword'";
+      $regno = mysqli_real_escape_string($db,$_POST['regno']);
+      $pass = mysqli_real_escape_string($db,$_POST['pass']); 
+      $sql = "SELECT * FROM user WHERE regNo = '$regno' and pass = '$pass'";
       $result = mysqli_query($db,$sql);
       $row = mysqli_fetch_array($result,MYSQLI_ASSOC);
       //$active = $row['active'];
@@ -16,16 +16,15 @@
       $count = mysqli_num_rows($result);
       
       // If result matched $myusername and $mypassword, table row must be 1 row
-		
       if($count == 1) {
-         //session_register("myusername");
-         echo "successfully logged in";
-         $_SESSION['login_user'] = $myusername;
+         //echo "successfully logged in";
+         $_SESSION['regno'] = $regno;
          header("location:../BS3/dashboard.html");
-
       }
       else {
          $error = "Your Login Name or Password is invalid";
+         echo "error";
+
       }
    }
 ?>
